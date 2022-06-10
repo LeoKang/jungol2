063efb5feeaaa68e10c176bbda4f2bd7fae13c32
LeoKang <xeungjin@naver.com>
2022-06-10 15:03:44 +0900

    1st commit
    Initial commit

---
+++ Main.java
@@ -0,0 +1,27 @@
+package p562;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] ar = new int[10];
+		for (int i = 0; i < ar.length; i++) {
+			ar[i] = sc.nextInt();
+		}
+		sc.close();
+
+		int odd = 0, even = 0;
+		for (int i = 0; i < ar.length; i++) {
+//			ar[0] : �汝鶾讕� 熱
+//			ar[1] : 礎熱廓簞 熱
+			if (i % 2 == 0) {
+				odd += ar[i];
+			} else {
+				even += ar[i];
+			}
+		}
+		System.out.println("sum : " + even);
+		System.out.printf("avg : %.1f\n", (double) odd / (ar.length / 2));
+	}
+}
