46cba54e94813cfb2d6460363f39395217226107
LeoKang <xeungjin@naver.com>
2022-06-10 15:06:00 +0900

    2nd commit
    Edit the text

--- Main.java
+++ Main.java
@@ -13,13 +13,11 @@ public class Main {
 
 		int odd = 0, even = 0;
 		for (int i = 0; i < ar.length; i++) {
-//			ar[0] : �汝鶾讕� 熱
-//			ar[1] : 礎熱廓簞 熱
-			if (i % 2 == 0) {
-				odd += ar[i];
-			} else {
-				even += ar[i];
-			}
+//			ar[0] : odd number
+//			ar[1] : even number
+			if (i % 2 == 0)		odd += ar[i];
+			else				even += ar[i];
+
 		}
 		System.out.println("sum : " + even);
 		System.out.printf("avg : %.1f\n", (double) odd / (ar.length / 2));
